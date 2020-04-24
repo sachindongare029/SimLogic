7e808e37ba7360fb65a70c005e4ff68984d62186
sachindongare029 <sachindongare029@gmail.com>
2020-04-24 14:09:52 +0530

    changes in routing

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
-import '../styles/home.scss';
+import '../styles/dashboard.scss';
 
-class Home extends React.Component {
+class Dashboard extends React.Component {
   render() {
     return (
       <div className="container">
         <h1>Hello world</h1>
         <FontAwesomeIcon icon={faThumbsUp} />
-        <Link to='/about'>About</Link>
       </div>
     );
   }
 }
 
-export default withRouter(Home);
+export default withRouter(Dashboard);
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import Home from "./components/Home";
-import About from './components/About';
+import Dashboard from "./components/Dashboard";
+// import About from './components/About';
 
 const Routes = () => (
   <React.Fragment>
     <main className="main">
-      <Route exact path={"/"} component={Home} />
-      <Route exact path={"/about"} component={About} />
+      <Route exact path={"/"} component={Dashboard} />
+      {/* <Route exact path={"/about"} component={About} /> */}
     </main>
   </React.Fragment>
 );

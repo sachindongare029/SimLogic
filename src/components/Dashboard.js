c5322b6b62a6bba844144d1cf56693634a1d98a9
sachindongare029 <sachindongare029@gmail.com>
2020-04-24 14:22:27 +0530

    navbar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React from "react";
 // import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
-import '../styles/dashboard.scss';
+import { withRouter } from "react-router";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import "../styles/dashboard.scss";
 
 class Dashboard extends React.Component {
   render() {
     return (
-      <div className="container">
-        <h1>Hello world</h1>
-        <FontAwesomeIcon icon={faThumbsUp} />
+      <div className="sl__dashboard">
+        <nav className="navbar navbar-light bg-light justify-content-between">
+          <a className="navbar-brand" href="/#">Navbar</a>
+          <form className="form-inline">
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </nav>
       </div>
     );
   }

956ac7b7265b89e7b3c2e95ef6a072cacb7f858e
sachindongare029 <sachindongare029@gmail.com>
2020-04-24 14:54:44 +0530

    navbar changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -9,18 +9,26 @@ class Dashboard extends React.Component {
   render() {
     return (
       <div className="sl__dashboard">
-        <nav className="navbar navbar-light bg-light justify-content-between">
-          <a className="navbar-brand" href="/#">Navbar</a>
+        <nav className="navbar navbar-light justify-content-between">
+          <a className="navbar-brand" href="/#">
+            Sim Logic
+          </a>
           <form className="form-inline">
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-              Search
-            </button>
+            <div className="basic-search">
+              <div className="input-field">
+                <input id="search" type="text" placeholder="Search" />
+                <div className="icon-wrap">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                  </svg>
+                </div>
+              </div>
+            </div>
           </form>
         </nav>
       </div>

2b43e88679a8e6024dd1a6fc7460d927da176851
sachindongare029 <sachindongare029@gmail.com>
2020-04-24 21:49:22 +0530

    inventory page added

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
-// import About from './components/About';
+import Inventory from './Inventory';
 
 const Routes = () => (
   <React.Fragment>
     <Route exact path={"/"} component={Dashboard} />
-    {/* <Route exact path={"/about"} component={About} /> */}
+    <Route exact path={"/inventory"} component={Inventory} />
   </React.Fragment>
 );
 

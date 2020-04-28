0c4336d2d0c1b14f7887ad1fc33bf778298687a3
sachindongare029 <sachindongare029@gmail.com>
2020-04-28 14:58:22 +0530

    card changes

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,52 +1,78 @@
-import React, { Component } from 'react'
-import "./index.scss"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./index.scss";
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard">
         <div className="dashboard-header">
-          <h4>status</h4>
-          <select id="menu" className="select-menu">
-            <option value="overview">overview</option>
-            <option value="data">data</option>
-          </select>
+          <h4>Dashboard</h4>
         </div>
-        <div className="row dashboard-content">
-          <div className="col-4">
-            <div className="data-card">
-              <h6>
-                <b>Inventory</b>
-                <i className="fa fa-info-circle" aria-hidden="true"></i>
-              </h6>
-              <p>206</p>
-              <button type="button" className="btn btn-default">
-                <i className="fa fa-arrow-right" aria-hidden="true"></i>
-              </button>
+        <div className="dashboard-content">
+          <div className="dashboard__section">Overview</div>
+          <div className="row">
+            <div className="col-4">
+              <div className="data-card">
+                <div className="card__statistics">
+                  <div className="statistics__info">
+                    <div className="statistics__title">
+                      <div className="card__title">Inventory</div>
+                      <i className="fa fa-info-circle" aria-hidden="true"></i>
+                    </div>
+                  </div>
+                  <div className="statistics__count">
+                    <div className="count">206</div>
+                  </div>
+                </div>
+                <div className="card__action--btn">
+                  <Link to="/" type="button" className="btn btn-default">
+                    <i className="fa fa-arrow-right" aria-hidden="true"></i>
+                  </Link>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="col-4">
-            <div className="data-card">
-              <h6>
-                <b>Data Usage</b>
-                <i className="fa fa-info-circle" aria-hidden="true"></i>
-              </h6>
-              <p>Total Data Used: 806.27 GB</p>
-              <button type="button" className="btn btn-default">
-                <i className="fa fa-arrow-right" aria-hidden="true"></i>
-              </button>
+            <div className="col-4">
+              <div className="data-card">
+                <div className="card__statistics">
+                  <div className="statistics__info">
+                    <div className="statistics__title">
+                      <div className="card__title">Data Usage</div>
+                      <i className="fa fa-info-circle" aria-hidden="true"></i>
+                    </div>
+                    <div className="count__info">Total Data Used</div>
+                  </div>
+                  <div className="statistics__count">
+                    <div className="count">806.27 <span className="count__extension">GB</span></div>
+                  </div>
+                </div>
+                <div className="card__action--btn">
+                  <button type="button" className="btn btn-default">
+                    <i className="fa fa-arrow-right" aria-hidden="true"></i>
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="col-4">
-            <div className="data-card">
-              <h6>
-                <b>Cost Summary</b>
-                <i className="fa fa-info-circle" aria-hidden="true"></i>
-              </h6>
-              <p>Total Cost: $5,497</p>
-              <button type="button" className="btn btn-default">
-                <i className="fa fa-arrow-right" aria-hidden="true"></i>
-              </button>
+            <div className="col-4">
+              <div className="data-card">
+                <div className="card__statistics">
+                  <div className="statistics__info">
+                    <div className="statistics__title">
+                      <div className="card__title">Cost Summary</div>
+                      <i className="fa fa-info-circle" aria-hidden="true"></i>
+                    </div>
+                    <div className="count__info">Total Cost</div>
+                  </div>
+                  <div className="statistics__count">
+                    <div className="count">$ 5,497</div>
+                  </div>
+                </div>
+                <div className="card__action--btn">
+                  <button type="button" className="btn btn-default">
+                    <i className="fa fa-arrow-right" aria-hidden="true"></i>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";

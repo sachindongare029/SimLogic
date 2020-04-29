bd68ae7f643237d8dfa898dcc13c253fdc6408f9
sachindongare029 <sachindongare029@gmail.com>
2020-04-29 21:17:41 +0530

    changes

--- src/Dashboard/InventaryInfo.js
+++ src/Dashboard/InventaryInfo.js
@@ -10,8 +10,17 @@ class InventaryInfo extends Component {
               <circle cx="20" cy="20" r="14" stroke-width="3" fill="#BE61CA" />
             </svg>
           </div>
-          <div className="chart__flow__stats"></div>
-          <div className="chart__flow__count"></div>
+          <div className="chart__flow__stats">
+            <div className="chart__stat__title">Activated</div>
+            <div className="chart__flow">
+              <div class="progress">
+                <div class="progress-bar" style={{width:"75%"}}>75%</div>
+              </div>
+            </div>
+          </div>
+          <div className="chart__flow__count">
+            143
+          </div>
         </div>
         <div className="chart__info__section">
           <div className="chart__color__logo">

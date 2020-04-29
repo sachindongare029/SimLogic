8034adb2f0f110c6c9d66f6e7a388caae4c7f6ac
sachindongare029 <sachindongare029@gmail.com>
2020-04-29 17:29:18 +0530

    card changes

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -74,19 +74,13 @@ class Dashboard extends Component {
                 <div className="data-card">
                   <div className="card__statistics">
                     <div className="statistics__info">
-                      <div className="statistics__title">
-                        <div className="card__title">Inventory</div>
-                        <i className="fa fa-info-circle" aria-hidden="true"></i>
-                      </div>
+                      <div className="statistics__title">Inventory</div>
                     </div>
-                    <div className="statistics__count">
-                      <div className="count">206</div>
+                    <div className="card__action--btn">
+                      <button type="button" className="btn btn-default">
+                        <i className="fa fa-arrow-right" aria-hidden="true"></i>
+                      </button>
                     </div>
-                  </div>
-                  <div className="card__action--btn">
-                    <Link to="/" type="button" className="btn btn-default">
-                      <i className="fa fa-arrow-right" aria-hidden="true"></i>
-                    </Link>
                   </div>
                 </div>
               </div>
@@ -94,22 +88,13 @@ class Dashboard extends Component {
                 <div className="data-card">
                   <div className="card__statistics">
                     <div className="statistics__info">
-                      <div className="statistics__title">
-                        <div className="card__title">Data Usage</div>
-                        <i className="fa fa-info-circle" aria-hidden="true"></i>
-                      </div>
-                      <div className="count__info">Total Data Used</div>
+                      <div className="statistics__title">Data Usage</div>
                     </div>
-                    <div className="statistics__count">
-                      <div className="count">
-                        806.27 <span className="count__extension">GB</span>
-                      </div>
+                    <div className="card__action--btn">
+                      <button type="button" className="btn btn-default">
+                        <i className="fa fa-arrow-right" aria-hidden="true"></i>
+                      </button>
                     </div>
-                  </div>
-                  <div className="card__action--btn">
-                    <button type="button" className="btn btn-default">
-                      <i className="fa fa-arrow-right" aria-hidden="true"></i>
-                    </button>
                   </div>
                 </div>
               </div>
@@ -117,20 +102,13 @@ class Dashboard extends Component {
                 <div className="data-card">
                   <div className="card__statistics">
                     <div className="statistics__info">
-                      <div className="statistics__title">
-                        <div className="card__title">Cost Summary</div>
-                        <i className="fa fa-info-circle" aria-hidden="true"></i>
-                      </div>
-                      <div className="count__info">Total Cost</div>
+                      <div className="statistics__title">Cost Summary</div>
                     </div>
-                    <div className="statistics__count">
-                      <div className="count">$ 5,497</div>
+                    <div className="card__action--btn">
+                      <button type="button" className="btn btn-default">
+                        <i className="fa fa-arrow-right" aria-hidden="true"></i>
+                      </button>
                     </div>
-                  </div>
-                  <div className="card__action--btn">
-                    <button type="button" className="btn btn-default">
-                      <i className="fa fa-arrow-right" aria-hidden="true"></i>
-                    </button>
                   </div>
                 </div>
               </div>

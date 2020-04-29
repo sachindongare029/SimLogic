57eabdaaf1fd4f521c77585362dbdda441c2c211
sachindongare029 <sachindongare029@gmail.com>
2020-04-29 23:09:04 +0530

    chart changes

--- src/Dashboard/InventaryInfo.js
+++ src/Dashboard/InventaryInfo.js
@@ -14,7 +14,7 @@ class InventaryInfo extends Component {
             <div className="chart__stat__title">Activated</div>
             <div className="chart__flow">
               <div class="progress">
-                <div class="progress-bar" style={{width:"75%"}}>75%</div>
+                <div class="progress-bar" style={{width:"75%"}}></div>
               </div>
             </div>
           </div>
@@ -28,8 +28,17 @@ class InventaryInfo extends Component {
               <circle cx="20" cy="20" r="14" stroke-width="3" fill="#633EBB" />
             </svg>
           </div>
-          <div className="chart__flow__stats"></div>
-          <div className="chart__flow__count"></div>
+          <div className="chart__flow__stats">
+            <div className="chart__stat__title">Activation Ready</div>
+            <div className="chart__flow">
+              <div class="progress">
+                <div class="progress-bar" style={{width:"45%"}}></div>
+              </div>
+            </div>
+          </div>
+          <div className="chart__flow__count">
+            39
+          </div>
         </div>
         <div className="chart__info__section">
           <div className="chart__color__logo">
@@ -37,8 +46,17 @@ class InventaryInfo extends Component {
               <circle cx="20" cy="20" r="14" stroke-width="3" fill="#F2BC5E" />
             </svg>
           </div>
-          <div className="chart__flow__stats"></div>
-          <div className="chart__flow__count"></div>
+          <div className="chart__flow__stats">
+            <div className="chart__stat__title">Deactivated</div>
+            <div className="chart__flow">
+              <div class="progress">
+                <div class="progress-bar" style={{width:"30%"}}></div>
+              </div>
+            </div>
+          </div>
+          <div className="chart__flow__count">
+            17
+          </div>
         </div>
         <div className="chart__info__section">
           <div className="chart__color__logo">
@@ -46,8 +64,17 @@ class InventaryInfo extends Component {
               <circle cx="20" cy="20" r="14" stroke-width="3" fill="#F13C59" />
             </svg>
           </div>
-          <div className="chart__flow__stats"></div>
-          <div className="chart__flow__count"></div>
+          <div className="chart__flow__stats">
+            <div className="chart__stat__title">Inventary</div>
+            <div className="chart__flow">
+              <div class="progress">
+                <div class="progress-bar" style={{width:"15%"}}></div>
+              </div>
+            </div>
+          </div>
+          <div className="chart__flow__count">
+            5
+          </div>
         </div>
         <div className="chart__info__section">
           <div className="chart__color__logo">
@@ -55,8 +82,17 @@ class InventaryInfo extends Component {
               <circle cx="20" cy="20" r="14" stroke-width="3" fill="#64C2A6" />
             </svg>
           </div>
-          <div className="chart__flow__stats"></div>
-          <div className="chart__flow__count"></div>
+          <div className="chart__flow__stats">
+            <div className="chart__stat__title">Retired</div>
+            <div className="chart__flow">
+              <div class="progress">
+                <div class="progress-bar" style={{width:"05%"}}></div>
+              </div>
+            </div>
+          </div>
+          <div className="chart__flow__count">
+            2
+          </div>
         </div>
       </div>
     );

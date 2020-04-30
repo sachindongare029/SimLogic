851b75ae87fff765fd9e81837a985b07bbcd1fc6
sachindongare029 <sachindongare029@gmail.com>
2020-04-30 12:36:21 +0530

    layout changes

--- src/Dashboard/InventaryInfo.js
+++ src/Dashboard/InventaryInfo.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class InventaryInfo extends Component {
   render() {
@@ -7,96 +7,111 @@ class InventaryInfo extends Component {
         <div className="chart__info__section">
           <div className="chart__color__logo">
             <svg height="100%" width="100%">
-              <circle cx="20" cy="20" r="14" stroke-width="3" fill="#BE61CA" />
+              <circle cx="20" cy="20" r="12" strokeWidth="3" fill="#BE61CA" />
             </svg>
           </div>
           <div className="chart__flow__stats">
             <div className="chart__stat__title">Activated</div>
             <div className="chart__flow">
-              <div class="progress">
-                <div class="progress-bar" style={{width:"75%"}}></div>
+              <div
+                className="progress"
+                data-toggle="tooltip"
+                data-placement="top"
+                title="75%"
+              >
+                <div className="progress-bar" style={{ width: "75%" }}></div>
               </div>
             </div>
           </div>
-          <div className="chart__flow__count">
-            143
-          </div>
+          <div className="chart__flow__count">143</div>
         </div>
         <div className="chart__info__section">
           <div className="chart__color__logo">
             <svg height="100%" width="100%">
-              <circle cx="20" cy="20" r="14" stroke-width="3" fill="#633EBB" />
+              <circle cx="20" cy="20" r="12" strokeWidth="3" fill="#633EBB" />
             </svg>
           </div>
           <div className="chart__flow__stats">
             <div className="chart__stat__title">Activation Ready</div>
             <div className="chart__flow">
-              <div class="progress">
-                <div class="progress-bar" style={{width:"45%"}}></div>
+              <div
+                className="progress"
+                data-toggle="tooltip"
+                data-placement="top"
+                title="45%"
+              >
+                <div className="progress-bar" style={{ width: "45%" }}></div>
               </div>
             </div>
           </div>
-          <div className="chart__flow__count">
-            39
-          </div>
+          <div className="chart__flow__count">39</div>
         </div>
         <div className="chart__info__section">
           <div className="chart__color__logo">
             <svg height="100%" width="100%">
-              <circle cx="20" cy="20" r="14" stroke-width="3" fill="#F2BC5E" />
+              <circle cx="20" cy="20" r="12" strokeWidth="3" fill="#F2BC5E" />
             </svg>
           </div>
           <div className="chart__flow__stats">
             <div className="chart__stat__title">Deactivated</div>
             <div className="chart__flow">
-              <div class="progress">
-                <div class="progress-bar" style={{width:"30%"}}></div>
+              <div
+                className="progress"
+                data-toggle="tooltip"
+                data-placement="top"
+                title="30%"
+              >
+                <div className="progress-bar" style={{ width: "30%" }}></div>
               </div>
             </div>
           </div>
-          <div className="chart__flow__count">
-            17
-          </div>
+          <div className="chart__flow__count">17</div>
         </div>
         <div className="chart__info__section">
           <div className="chart__color__logo">
             <svg height="100%" width="100%">
-              <circle cx="20" cy="20" r="14" stroke-width="3" fill="#F13C59" />
+              <circle cx="20" cy="20" r="12" strokeWidth="3" fill="#F13C59" />
             </svg>
           </div>
           <div className="chart__flow__stats">
             <div className="chart__stat__title">Inventary</div>
             <div className="chart__flow">
-              <div class="progress">
-                <div class="progress-bar" style={{width:"15%"}}></div>
+              <div
+                className="progress"
+                data-toggle="tooltip"
+                data-placement="top"
+                title="15%"
+              >
+                <div className="progress-bar" style={{ width: "15%" }}></div>
               </div>
             </div>
           </div>
-          <div className="chart__flow__count">
-            5
-          </div>
+          <div className="chart__flow__count">5</div>
         </div>
         <div className="chart__info__section">
           <div className="chart__color__logo">
             <svg height="100%" width="100%">
-              <circle cx="20" cy="20" r="14" stroke-width="3" fill="#64C2A6" />
+              <circle cx="20" cy="20" r="12" strokeWidth="3" fill="#64C2A6" />
             </svg>
           </div>
           <div className="chart__flow__stats">
             <div className="chart__stat__title">Retired</div>
             <div className="chart__flow">
-              <div class="progress">
-                <div class="progress-bar" style={{width:"05%"}}></div>
+              <div
+                className="progress"
+                data-toggle="tooltip"
+                data-placement="top"
+                title="5%"
+              >
+                <div className="progress-bar" style={{ width: "05%" }}></div>
               </div>
             </div>
           </div>
-          <div className="chart__flow__count">
-            2
-          </div>
+          <div className="chart__flow__count">2</div>
         </div>
       </div>
     );
   }
 }
 
-export default InventaryInfo
+export default InventaryInfo;
